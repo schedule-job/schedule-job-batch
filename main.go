7bb9817e2a587bcc72442c8bef3507efe3007e56
Sotaneum <gnyontu39@gmail.com>
2024-10-03 22:49:04 +0900

    feat. DB 연결 추가

--- main.go
+++ main.go
@@ -1,5 +1,83 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+
+	parser "github.com/Sotaneum/go-args-parser"
+	"github.com/gin-gonic/gin"
+	ginsession "github.com/go-session/gin-session"
+	"github.com/schedule-job/schedule-job-batch/internal/pg"
+)
+
+type Options struct {
+	Port           string
+	PostgresSqlDsn string
+	TrustedProxies string
+}
+
+var DEFAULT_OPTIONS = map[string]string{
+	"PORT":             "8080",
+	"POSTGRES_SQL_DSN": "",
+	"TRUSTED_PROXIES":  "",
+}
+
+func getOptions() *Options {
+	rawOptions := parser.ArgsJoinEnv(DEFAULT_OPTIONS)
+
+	options := new(Options)
+	options.Port = rawOptions["PORT"]
+	options.PostgresSqlDsn = rawOptions["POSTGRES_SQL_DSN"]
+	options.TrustedProxies = rawOptions["TRUSTED_PROXIES"]
+
+	return options
+}
+
+func safeGo(f func()) {
+	go func() {
+		defer func() {
+			if r := recover(); r != nil {
+				fmt.Println("Recovered from panic:", r)
+			}
+		}()
+		f()
+	}()
+}
+
 func main() {
-	
+	options := getOptions()
+	if len(options.PostgresSqlDsn) == 0 {
+		panic("not found 'POSTGRES_SQL_DSN' options")
+	}
+	if len(options.Port) == 0 {
+		panic("not found 'PORT' options")
+	}
+
+	database := pg.New(options.PostgresSqlDsn)
+
+	router := gin.Default()
+	router.Use(ginsession.New())
+
+	if options.TrustedProxies != "" {
+		trustedProxies := strings.Split(options.TrustedProxies, ",")
+		router.SetTrustedProxies(trustedProxies)
+	}
+
+	router.POST("/api/v1/schedule/next-schedule", func(ctx *gin.Context) {
+		// - 특정 아이템의 다음 실행일 API
+		ctx.JSON(200, gin.H{"code": 200, "data": "ok"})
+	})
+
+	router.POST("/api/v1/schedule", func(ctx *gin.Context) {
+		// - 1000개씩 읽어서 작업을 진행. 모든 요청이 끝나야 다음 1000개 작업
+		ctx.JSON(200, gin.H{"code": 200, "data": "ok"})
+	})
+
+	router.NoRoute(func(ctx *gin.Context) {
+		ctx.JSON(404, gin.H{"code": 404, "message": "접근 할 수 없는 페이지입니다!"})
+	})
+
+	fmt.Println("Started Agent! on " + options.Port)
+
+	router.Run(":" + options.Port)
 }
